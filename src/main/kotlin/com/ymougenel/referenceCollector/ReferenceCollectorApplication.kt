05d4c35601a0854df4eb18011552bbd4455c4590
ymougenel <yann.mougenel@gmail.com>
2019-02-11 23:23:04 +0100

    init project

---
+++ src/main/kotlin/com/ymougenel/referenceCollector/ReferenceCollectorApplication.kt
@@ -0,0 +1,12 @@
+package com.ymougenel.referenceCollector
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReferenceCollectorApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReferenceCollectorApplication>(*args)
+}
+
