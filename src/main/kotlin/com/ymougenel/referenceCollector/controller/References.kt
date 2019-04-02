490e928dd376b23edf2cc3ed8fedba84b0f8e2a8
Yann <yann.mougenel@gmail.com>
2019-04-02 17:08:52 +0200

    add pagination feature

--- src/main/kotlin/com/ymougenel/referenceCollector/controller/References.kt
+++ src/main/kotlin/com/ymougenel/referenceCollector/controller/References.kt
@@ -15,12 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import javax.validation.Valid
+import java.util.stream.Collectors
+import java.util.stream.IntStream
+
+
 
 @Controller
 @RequestMapping("/references")
 class References {
     private lateinit var referencesDao: ReferenceDAO
-
+    private var PAGINATION_RANGE = 3;
     @Autowired
     constructor(ReferencesDAO: ReferenceDAO) {
         this.referencesDao = ReferencesDAO
@@ -46,9 +50,35 @@ class References {
     @GetMapping
     fun paginate(model: Model,
                  @RequestParam("page", required = false, defaultValue = "0") page: Int,
-                 @RequestParam("size", required = false, defaultValue = "5") size: Int): String {
-        val findAll = referencesDao.findAll(PageRequest.of(page, size))
+                 @RequestParam("size", required = false, defaultValue = "2") size: Int,
+                 @RequestParam("direction", required = false, defaultValue = "ASC") direction: String,
+                 @RequestParam("filterBy", required = false, defaultValue = "id") filterBy: String
+                ): String {
+        val findAll = referencesDao.findAll(PageRequest.of(page, size, Sort.Direction.fromString(direction), filterBy))
         model.addAttribute("page", findAll)
+        model.addAttribute("direction", direction)
+
+        if (findAll.totalPages > 0) {
+            val pageIndex = findAll.number +1
+            val minRange: Int
+            val maxRange: Int
+            if (pageIndex == 1) {
+                minRange = 1;
+                maxRange = Math.min(PAGINATION_RANGE, findAll.totalPages)
+            }
+            else if (pageIndex >= findAll.totalPages) {
+                maxRange = findAll.totalPages
+                minRange = Math.max(1,findAll.totalPages - PAGINATION_RANGE + 1)
+            }
+            else {
+                minRange = pageIndex - PAGINATION_RANGE / 2
+                maxRange = pageIndex + PAGINATION_RANGE / 2
+            }
+            val pageNumbers = IntStream.range(minRange, maxRange +1)
+                    .boxed()
+                    .collect(Collectors.toList())
+            model.addAttribute("pageNumbers", pageNumbers)
+        }
         return "refList"
     }
 }
