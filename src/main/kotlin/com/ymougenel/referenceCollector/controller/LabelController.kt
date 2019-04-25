e16dc4fdb3f680610a4794dbfa9090b300f675c8
Yann <yann.mougenel@gmail.com>
2019-04-25 13:29:08 +0200

    feature: update/delete Label

--- src/main/kotlin/com/ymougenel/referenceCollector/controller/LabelController.kt
+++ src/main/kotlin/com/ymougenel/referenceCollector/controller/LabelController.kt
@@ -1,14 +1,14 @@
 package com.ymougenel.referenceCollector.controller
 
 import com.ymougenel.referenceCollector.model.Label
+import com.ymougenel.referenceCollector.model.Reference
 import com.ymougenel.referenceCollector.persistence.LabelDAO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.validation.Errors
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @Controller
 @RequestMapping("/labels")
@@ -21,15 +21,36 @@ class LabelController {
         this.labelDAO = labelDAO
     }
 
+
     @GetMapping
-    fun getLabels(model: Model): String {
-        model.addAttribute("labels", labelDAO.findAll())
-        return "labelList"
+    fun getLabels(model: Model,
+                 @RequestParam("mode", required = false, defaultValue = "list") mode: String,
+                 @RequestParam("id", required = false, defaultValue = "0") id: Long): String {
+
+        model.addAttribute("labels", labelDAO.findAll().sortedBy { it.name })
+
+        // Retrieve label or create new one
+        val label: Label = if (id!=0L) labelDAO.findById(id).get() else Label()
+
+        model.addAttribute("label",label)
+        model.addAttribute("mode", mode)
+        return "label_list"
     }
 
     @PostMapping
-    fun getLabels(model: Model, @RequestParam("name") name: String): String {
-        this.labelDAO.save(Label(0L,name,null))
-        return "labelList"
+    fun updateLabel(@Valid label: Label, errors: Errors): String {
+        //TODO form mapping
+        if(errors.hasErrors()) {
+            //TODO error
+            return "redirect:/labels"
+        }
+        labelDAO.save(label)
+        return "redirect:/labels"
+    }
+
+    @GetMapping("/delete")
+    fun deleteLabel(model: Model, @RequestParam("id") id: Long): String {
+        labelDAO.deleteById(id)
+        return "redirect:/labels"
     }
 }
